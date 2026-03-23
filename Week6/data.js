b5eb0e138ad0f68af716aa6c75da5e3bd4e74347
ishrat <90342441+ishtotheizzo@users.noreply.github.com>
2026-03-23 11:06:22 -0700

    Added data set for master share

---
+++ Week6/data.js
@@ -0,0 +1,23 @@
+// pokemon sources https://pokemondb.net/sprites 
+
+const pokemon = [
+    {name: 'Pikachu', 
+        type: 'Electric',
+        image: '<a href="https://pokemondb.net/pokedex/pikachu"><img src="https://img.pokemondb.net/sprites/x-y/normal/pikachu-f.png" alt="Pikachu"></a>'},
+
+        {name: 'Gengar', 
+        type: 'Ghost',
+        image: '<a href="https://pokemondb.net/pokedex/gengar"><img src="https://img.pokemondb.net/sprites/x-y/normal/gengar.png" alt="Gengar"></a>'},
+
+        {name: 'Dragonite', 
+        type: 'Dragon',
+        image: '<a href="https://pokemondb.net/pokedex/dragonite"><img src="https://img.pokemondb.net/sprites/x-y/normal/dragonite.png" alt="Dragonite"></a>'},
+
+        {name: 'Lapras', 
+        type: 'Water',
+        image: '<a href="https://pokemondb.net/pokedex/lapras"><img src="https://img.pokemondb.net/sprites/x-y/normal/lapras.png" alt="Lapras"></a>'},
+
+          {name: 'Umbreon', 
+        type: 'Dark',
+        image: '<a href="https://pokemondb.net/pokedex/umbreon"><img src="https://img.pokemondb.net/sprites/x-y/normal/umbreon.png" alt="Umbreon"></a>'}
+]
